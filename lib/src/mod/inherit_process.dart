511a06a16a8c046daf2ce309fdd00adc50cd020d
sanfan.hx <sanfan.hx@alibaba-inc.com>
2019-02-10 10:45:58 +0800

    Create inherit_process.dart

---
+++ lib/src/mod/inherit_process.dart
@@ -0,0 +1,31 @@
+//
+// Created with Android Studio.
+// User: 三帆
+// Date: 07/02/2019
+// Time: 21:14
+// email: sanfan.hx@alibaba-inc.com
+// tartget:  xxx
+//
+
+import 'package:flutter/material.dart';
+import 'picker_popup_route.dart';
+
+class InheritRouteWidget extends InheritedWidget {
+  final CityPickerRoute router;
+
+  InheritRouteWidget({
+    Key key,
+    @required this.router,
+    Widget child
+  }) : super(key: key, child: child);
+
+  static InheritRouteWidget of(BuildContext context) {
+    return context.inheritFromWidgetOfExactType(InheritRouteWidget);
+  }
+
+  @override
+  bool updateShouldNotify(InheritRouteWidget oldWidget) {
+    // TODO: implement updateShouldNotify
+    return  oldWidget.router != router;
+  }
+}
