e2809f542ebdee13611495c83e49af5fcbefb91c
ipcjs.mac <gipcjs@gmail.com>
2023-07-05 15:13:48 +0800

    chore: 关闭调试日志

--- lib/src/utils/utils.dart
+++ lib/src/utils/utils.dart
@@ -3,7 +3,7 @@ import 'package:flutter/foundation.dart';
 /// Created by ipcjs on 2023/7/5.
 
 /// 控制是否显示调试用的输出
-const kVerbose = true && kDebugMode;
+const kVerbose = false && kDebugMode;
 
 extension ListExt<E> on List<E> {
   /// copy from [elementAtOrNull]
